a33487de60ba050e4e5861b09d2d94de26de15c5
MaVegaDev <manuel.vega.dev@proton.me>
2026-08-06 15:29:39 -0600

    fix

---
+++ app/models/technician.py
@@ -0,0 +1,3 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
+from app.core.database import Base
---
+++ app/models/user.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+from app.core.database import Base
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(String, unique=True, index=True, nullable=false)
+    name = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    role = Column(String, default='client') #Only the roles 'Admin', 'technician' and client
+    is_active = Column(Boolean, default=True)
+
+    technician = relationship('Technician', back_populates='user', uselist=False)
+    service_orders = relationship('ServiceOrder', back_populates='client', foreign_keys="ServiceOrder.client_id")
